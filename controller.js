d8997e3837528e51f65ee9c0a3f07a872dc307cf
Puppey23 <grandevalore.23@gmail.com>
2021-01-05 16:37:44 +0700

    berhasil menampilkan semua data

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function (req, res) {
     response.ok('Aplikasi REST API Berjalan', res);
 };
+
+//menampilkan semua data
+exports.tampilAll = function (req, res) {
+    connection.query('SELECT * FROM formulir_pengajuan_ppdb', function (error, rows, fileds) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
+};
